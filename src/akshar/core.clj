fa880b3fc34bf8ed6e3ad9f915fd0dc75479491f
Mithun <udhan@users.noreply.github.com>
2016-04-09 17:32:00 -0700

    basic frame

--- src/akshar/core.clj
+++ src/akshar/core.clj
@@ -1,7 +1,26 @@
 (ns akshar.core
+  (:import (javax.swing JFrame JTextPane))
   (:gen-class))
 
+(defn make-text-pane []
+   (JTextPane.))
+
+(defn make-frame []
+  (doto (JFrame. "Akshar")
+    (.setSize 200 200)))
+
+(defn make-editor []
+  (let [frame (make-frame)
+        tp (make-text-pane)
+        cp (.getContentPane frame)]
+    (.add cp tp)
+    {:frame frame :tp tp :cp cp}
+    ))
+
+(defn start []
+  (def editor (make-editor))
+  (.setVisible (:frame editor) true))
+    
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (start))
