17d697ef723893ce3d7bb73e9717d9f14a172c03
Mithun <udhan@users.noreply.github.com>
2016-04-12 01:03:40 -0700

    working save

--- src/akshar/core.clj
+++ src/akshar/core.clj
@@ -1,7 +1,7 @@
 (ns akshar.core
   (:import (javax.swing JFrame JTextPane Action
                         KeyStroke AbstractAction
-                        JScrollPane JPanel)
+                        JScrollPane JPanel JComponent)
            (javax.swing.text DocumentFilter)
            (java.awt.event KeyListener KeyEvent)
            (java.awt BorderLayout Dimension)
@@ -11,7 +11,10 @@
 
 ;; State
 
-(def state (atom {:mode :cmd :chord ""}))
+(def state (atom {:wid 0 :mode :cmd :chord ""}))
+
+(defn get-next-wid []
+  (:wid (swap! state update-in [:wid] inc)))
 
 (defn add-chord-key! [key]
   (swap! state update-in [:chord] (partial str key)))
@@ -20,6 +23,7 @@
   (keyword (get @state :chord "not-found")))
 
 (defn set-mode! [mode]
+  (println "Mode:" mode)
   (swap! state assoc :chord "")
   (swap! state assoc :mode mode))
 
@@ -29,16 +33,17 @@
 (defn cmd? []
   (= (:mode @state) :cmd))
 
-(defn insert-mode []
+(defn insert-mode [& args]
   (set-mode! :ins))
 
 ;; File operations
 
-(defn open [file window]
-  (.read (:b window) (FileReader. (.getAbsolutePath file)) nil))
+(defn open [window]
+  (.read (:b window) (FileReader. (.getAbsolutePath (:file window))) nil))
 
-(defn save! [file window]
-  (.write (:b window) (FileWriter. file)))
+(defn save! [event]
+  (println event)
+  (.write (:b (:window event)) (FileWriter. (:file (:window event)))))
 
 ;; Text pane related operations
 
@@ -57,7 +62,7 @@
     (.getText doc soff (- eoff soff))))
 
 (defn eval-line [event]
-  (append-text 
+  (append-text
    (with-out-str
      (try
        (load-string
@@ -74,8 +79,13 @@
 
 (defn handle-chords [event]
   (add-chord-key! (:ch event))
+  ;;(println (.getDocumentProperties (:doc event)))
   (if-let [f (get @chords (get-chord))]
-    (f event)))
+    (let [window (first
+                    (filter
+                      #(= (.getProperty (:doc event) "wid") (:wid %))
+                       (:windows @state)))]
+    (f (assoc event :window window)))))
 
 ;; Actions
 
@@ -98,49 +108,52 @@
 
 ;; UI elements
 
-(defn add-keybindings [frame]
-  (let [imap (.getInputMap (.getContentPane frame))]
+(defn add-keybindings [window]
+  (let [imap (.getInputMap (:b window) JComponent/WHEN_IN_FOCUSED_WINDOW)]
     (.put imap (KeyStroke/getKeyStroke KeyEvent/VK_ESCAPE 0) esc-action)))
 
 (defn make-text-pane []
    (JTextPane.))
 
-(defn make-window []
+(defn make-window [fpath]
   (let [panel (JPanel.)
         tag (make-text-pane)
-        body (make-text-pane)]
+        body (make-text-pane)
+        file (io/as-file fpath)
+        wid (get-next-wid)]
     (.setPreferredSize tag (Dimension. 100 100))
     (.setLayout panel (BorderLayout.))
     (.add panel (JScrollPane. tag) BorderLayout/PAGE_START)
     (.add panel (JScrollPane. body) BorderLayout/CENTER)
-    {:p panel :t tag :b body}))
-  
+    {:p panel :t tag :b body :file file :wid wid}))
+
 (defn make-frame []
-  (let [frame (JFrame. "Akshar")] 
-    (add-keybindings frame)
+  (let [frame (JFrame. "Akshar")]
     (doto frame
       (.setSize 200 200)
-      (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE))))
+      (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
+      )))
 
 (defn make-editor [fpath]
   (let [frame (make-frame)
-        window (make-window)
-        file (io/as-file fpath)
-        res (open file window)]
+        window (make-window fpath)
+        res (open window)]
     (.add (.getContentPane frame) (:p window) BorderLayout/CENTER)
     (.setDocumentFilter (.getDocument (:b window)) doc-filter)
+    (.putProperty (.getDocument (:b window)) "wid" (:wid window))
+    (add-keybindings window)
     (.pack frame)
+    (println (.getDocumentProperties (.getDocument (:b window))))
     {:frame frame
      :windows [window]}))
 
-  
-
 (defn start [fpath]
   (let [editor (make-editor fpath)]
+  (println editor)
   (.setVisible (:frame editor) true)
   (swap! state merge editor)
   (insert-mode)))
-    
+
 (defn -main
   [fpath & args]
   (start fpath))
